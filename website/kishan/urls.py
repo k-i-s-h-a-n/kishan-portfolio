cc08b6714e2856e428758c530e5aee1f0ff59708
kishan <kishank@okcl.org>
2024-01-16 14:54:33 +0530

    collectstatic

--- website/kishan/urls.py
+++ website/kishan/urls.py
@@ -4,5 +4,6 @@ from kishan.views import *
 
 urlpatterns = [
     path("profile/",profile,name="profile"),
+    path("",profile,name="profile"),
     path("profile/send_msg",send_msg,name="send_msg"),
 ]
